83abf8567126baff87ff4b11e32c0e19dfd87b1e
Patrick Ndifon <pmndifon@gmail.com>
2019-07-23 03:05:10 +0100

    Fetch quotes from github gist api

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import './App.css';
 import Button from './components/Button';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex: null
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+      .then(data => data.json())
+      .then(quotes => this.setState({ quotes }));
+  }
+
+  nextQuoteClickHandler() {
+    console.log('Hi')
+  }
+
   render() {
+    console.log(this.state.quotes);
     return (
       <div className="App" id="quote-box">
-        <Button buttonDisplayName="Next Quote" />
+        <Button buttonDisplayName="Next Quote" clickHandler={this.nextQuoteClickHandler}/>
       </div>
     );
   }
